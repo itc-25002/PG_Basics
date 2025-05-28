73a17508fae33b3e1d1e0c600c1941ab006e8f8d
itc-25002 <n25002@std.it-college.ac.jp>
2025-05-28 03:40:00 +0000

    P87 第5章 チャレンジ問題 終了

---
+++ Chapter05/challenge5-01.py
@@ -0,0 +1,6 @@
+#好きなミュージシャンのリストを作ろう
+favorite_musician = ["Janne Da Arc","稲葉曇",]
+
+print(favorite_musician)
+
+
---
+++ Chapter05/challenge5-05.py
@@ -0,0 +1,7 @@
+#自分が好きなミュージシャンを辞書のキーに入れ、そのバリューとしてそのミュージシャンの曲をリストで持たせよう。
+
+song1 = ["ダイヤモンドヴァージン","月光花"]
+song2 = ["リレイアウター","アイペース"]
+
+favorite_musician = {"Janne Da Arc":song1,"稲葉曇":song2}
+print(favorite_musician)
